cfee71d05086e3657fbdac1d1126614e12eed591
Mohamad Agung Faisal <mohamadagungfaisal@gmail.com>
2023-09-14 20:54:36 +0700

    add components

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -4,16 +4,67 @@ import React, { useTransition, useState } from 'react'
 import Image from 'next/image'
 import TabButton from './TabButton'
 
+const TAB_DATA = [
+    {
+        tittle: "Skills",
+        id: "skills",
+        content: (
+            <ul className="grid grid-cols-2 gap-2 list-disc pl-2">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>React Js</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+                <li>CorelDraw</li>
+                <li>Adobe Photoshop</li>
+                <li>Microsoft Office</li>
+            </ul>
+        )
+    },
+    {
+        tittle: "Education",
+        id: "education",
+        content: (
+            <ul className="grid grid-cols-2 gap-2 list-disc pl-2">
+                <li>SD Islam Riyadlul Mubtadiin Kedok</li>
+                <li>MTs Negeri Malang 3</li>
+                <li>MA Negeri Gondanglegi</li>
+            </ul>
+        )
+    },
+    {
+        tittle: "certifications",
+        id: "certifications",
+        content: (
+            <ul className="grid grid-cols-2 gap-2 list-disc pl-2">
+                <li>Front End Developer Academy Code</li>
+            </ul>
+        )
+    },
+    {
+        tittle: "Experience",
+        id: "experience",
+        content: (
+            <ul className="grid grid-cols-2 gap-2 list-disc pl-2">
+                <li>Front End Developer Academy Code</li>
+                <li>Front End Developer Academy Code</li>
+                <li>Front End Developer Academy Code</li>
+            </ul>
+        )
+    },
+]
+
 const AboutSection = () => {
 
-    const [tab, setTab] = useState("Skilss");
+    const [tab, setTab] = useState("skills");
     const [isPending, startTransition] = useTransition();
-
+    
     const handleTabChange = (id) => {
-        startTransition(() =>
-            setTab(id)
-        );
-    }
+        startTransition(() => {
+        setTab(id);
+        });
+    };
     
     return (
         <section>
@@ -39,7 +90,7 @@ const AboutSection = () => {
                     <p className="text-justify indent-8 py-2 text-sm lg:text-base">
                         I am a fast learner and I am always looking to expand my knowledge and skills. I have skills in graphic design, logo & branding as well as software skills such as Adobe Photoshop, CorelDraw and Microsoft Office. Recently I have acquired a new skill, namely user interface design for websites and mobile, using the programming languages HTML, CSS, JavaScript and using the Node.js Framework and React.js as a library.
                     </p>
-                    <div className="flex flex-row justify-start mt-8">
+                    <div className="flex flex-row justify-beetween space-x-6 mt-8">
                         <TabButton
                             selectTab={() => handleTabChange("skills")}
                             active={tab === "skills"}
@@ -62,12 +113,15 @@ const AboutSection = () => {
                             Certifications{" "}
                         </TabButton>
                         <TabButton
-                            selectTab={() => handleTabChange("Experience")}
-                            active={tab === "Experience"}
+                            selectTab={() => handleTabChange("experience")}
+                            active={tab === "experience"}
                         >
                             {" "}
                             Experience {" "}
                         </TabButton>
+                    </div>
+                    <div className="mt-8 text-left">
+                        {TAB_DATA.find((t) => t.id === tab).content}
                     </div>
                 </div>
             </div>
